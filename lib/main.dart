faa47f3e485e348869ba1ead2c7dab9028967e92
muhammadyaumil1212 <yaumilramkhadani@gmail.com>
2022-04-05 13:24:33 +0700

    membuat splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:berguru_app/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Berguru',
-      home: ,
+      home: SplashScreen(),
     );
   }
 }
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -10,7 +10,7 @@ class SplashScreen extends StatelessWidget{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
+            Image.asset("assets/Logo/berguru_logo.png"),
           ],
         ),
       ),
