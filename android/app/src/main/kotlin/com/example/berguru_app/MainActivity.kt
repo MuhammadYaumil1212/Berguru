f850067dbb37f4fcce371c81d82e84a16e859f74
muhammadyaumil1212 <yaumilramkhadani@gmail.com>
2022-04-05 13:11:15 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/berguru_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.berguru_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff5D8EE2),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+      );
+  }
+
+}
